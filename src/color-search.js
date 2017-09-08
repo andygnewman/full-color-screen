f3233c4dca2aed469d29d0f37f8de5bfb00a75e3
Andy <andygnewman@gmail.com>
2017-09-08 20:17:25 +0100

    base case initial colour object

--- src/color-search.js
+++ src/color-search.js
@@ -1,15 +1,19 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-const SEARCH_SITE_DEFAULT = 'https://www.e-paint.co.uk/search_colour.asp';
+const SEARCH_SITE_HOST = 'https://www.e-paint.co.uk/';
+const SEARCH_SITE_ROUTE = 'search_colour.asp';
+const NEWLINE_REGEX = /\n|\r/g;
+const TRAILING_SPACE_REGEX = /\s+$/;
+const RANGE_REGEX = /(?:^\s*Colour Range: )(.*?)(?=Colour reference)/;
+const DESCRIPTION_REGEX = /(?:Description:       )(.*?)(?=View L)/;
 
-const constructUrl = (searchText, searchSite = SEARCH_SITE_DEFAULT) => {
-  const queryParam = searchSite === SEARCH_SITE_DEFAULT ? 'cQuery' : 'q';
-  const url = `${searchSite}?${queryParam}=${searchText}`;
+const constructUrl = (searchText, searchSite = SEARCH_SITE_HOST) => {
+  const queryParam = searchSite === SEARCH_SITE_HOST ? 'cQuery' : 'q';
+  const url = `${searchSite}${SEARCH_SITE_ROUTE}?${queryParam}=${searchText}`;
   return url;
 };
 
 const fetchSearch = (url) => {
-  console.log('URL: ', url);
   const options = {
     method: 'POST'
   };
@@ -22,16 +26,19 @@ const fetchSearch = (url) => {
 const extractLinkToValues = (responseText) => {
   const resultSet = [];
   const $ = cheerio.load(responseText, {
-    ignoreWhitespace: true,
-    xmlMode: true
+    ignoreWhitespace: true
   });
   const results = $('.information');
-  console.log('Length: ', $('.results').length);
   results.each((i, el) => {
-    console.log('Iteration: ', i, ' content', $(this).children('b'));
-    resultSet.push($(this).html());
+    const text = $(el).text().replace(NEWLINE_REGEX, "");
+    console.log('Iteration: ', i, ', Text: ', text);
+    const resultObject = {
+      range: RANGE_REGEX.exec(text)[1].replace(TRAILING_SPACE_REGEX, ""),
+      description: DESCRIPTION_REGEX.exec(text)[1].replace(TRAILING_SPACE_REGEX, ""),
+      link: SEARCH_SITE_HOST + $(el).children('a').attr('href')
+    };
+    resultSet.push(resultObject);
   });
-  // console.log('Cheerio ResultSet: ', resultSet);
   return resultSet;
 };
 
