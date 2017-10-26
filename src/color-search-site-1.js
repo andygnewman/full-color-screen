4eb8213d0982ed54e380c7f943d5014e337c1dd2
Andy <andygnewman@gmail.com>
2017-10-26 15:35:01 +0100

    abstract out color search site 1

---
+++ src/color-search-site-1.js
@@ -0,0 +1,90 @@
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+const SEARCH_SITE_ROUTE = 'search_colour.asp';
+const SEARCH_SITE_QUERY_PARAM = 'cQuery';
+const NEWLINE_REGEX = /\n|\r/g;
+const TRAILING_SPACE_REGEX = /\s+$/;
+const RANGE_REGEX = /(?:^\s*Colour Range: )(.*?)(?=Colour reference)/;
+const NAME_REGEX = /(?:Description:       )(.*?)(?=View)/;
+const MAX_RESULTS = 5;
+
+const constructUrl = (searchText, searchHost) => {
+  const queryParam = 'cQuery';
+  const url = `${searchHost}${SEARCH_SITE_ROUTE}?${SEARCH_SITE_QUERY_PARAM}=${searchText}`;
+  return url;
+};
+
+const fetchSearch = (url) => {
+  const options = {
+    method: 'POST'
+  };
+  return fetch(url, options)
+    .then(response => {
+      return response.text();
+    });
+};
+
+const extractLinkToValues = (responseText, maxResults, searchHost) => {
+  const resultSet = [];
+  const $ = cheerio.load(responseText, {
+    ignoreWhitespace: true
+  });
+  const results = $('.information');
+  results.each((i, el) => {
+    const text = $(el).text().replace(NEWLINE_REGEX, '');
+    const resultObject = {
+      range: RANGE_REGEX.exec(text)[1].replace(TRAILING_SPACE_REGEX, ''),
+      name: NAME_REGEX.exec(text)[1].replace(TRAILING_SPACE_REGEX, ''),
+      link: searchHost + $(el).children('a').last().attr('href')
+    };
+    resultSet.push(resultObject);
+    if (i === (maxResults -1)) return false;
+  });
+  const resultMaxNumber = results.length > maxResults ? maxResults : false;
+  return {resultSet, resultMaxNumber};
+};
+
+const extractRGBValues = (colourPages) => {
+  const rgbValues = [];
+  colourPages.forEach(page => {
+    const $ = cheerio.load(page, {
+      ignoreWhitespace: true
+    });
+    const rgbValue = $('.lab-result').children('span').eq(2)
+      .children('div').eq(2)
+      .children('p').eq(1)
+      .text()
+      .replace(/\s/g, '')
+      .replace(/sRGB\:/, '')
+      .split(';');
+    rgbValues.push(rgbValue);
+  });
+  return rgbValues;
+};
+
+const getRGBValues = (resultObject) => {
+  const {resultSet, resultMaxNumber} = resultObject;
+  const promises = [];
+  const links = resultSet.map(result => result.link);
+  links.forEach(link => {
+    promises.push(fetchSearch(link));
+  });
+  return Promise.all(promises)
+    .then(colourPages => {
+      const rgbValues = extractRGBValues(colourPages);
+      resultSet.forEach((result, index) => result.rgb = rgbValues[index]);
+      return {resultSet, resultMaxNumber};
+    });
+};
+
+const returnResponse = (resultObject) => {
+  return resultObject;
+};
+
+module.exports = (searchText, searchHost, maxResults) => {
+  const url = constructUrl(searchText, searchHost);
+  return fetchSearch(url)
+    .then(responseText => extractLinkToValues(responseText, maxResults, searchHost))
+    .then(resultObject => getRGBValues(resultObject))
+    .then(resultObject => returnResponse(resultObject))
+};
--- src/color-search.js
+++ src/color-search.js
@@ -1,90 +1,18 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
-const SEARCH_SITE_HOST = 'https://www.e-paint.co.uk/';
-const SEARCH_SITE_ROUTE = 'search_colour.asp';
-const NEWLINE_REGEX = /\n|\r/g;
-const TRAILING_SPACE_REGEX = /\s+$/;
-const RANGE_REGEX = /(?:^\s*Colour Range: )(.*?)(?=Colour reference)/;
-const NAME_REGEX = /(?:Description:       )(.*?)(?=View)/;
+const colorSearch1 = require('./color-search-1');
+const colorSearch2 = require('./color-search-2');
+
+const SEARCH_SITE_HOST_1 = 'https://www.e-paint.co.uk/';
+const SEARCH_SITE_HOST_2 = 'http://encycolorpedia.com/'
 const MAX_RESULTS = 5;
 
-const constructUrl = (searchText, searchSite = SEARCH_SITE_HOST) => {
-  const queryParam = searchSite === SEARCH_SITE_HOST ? 'cQuery' : 'q';
-  const url = `${searchSite}${SEARCH_SITE_ROUTE}?${queryParam}=${searchText}`;
-  return url;
-};
+module.exports = (searchText, searchHost) => {
 
-const fetchSearch = (url) => {
-  const options = {
-    method: 'POST'
-  };
-  return fetch(url, options)
-    .then(response => {
-      return response.text();
-    });
-};
+  if (searchHost === SEARCH_SITE_HOST_1) {
+    return colorSearch1(searchText, SEARCH_SITE_HOST_1, MAX_RESULTS);
+  } else if (searchHost === SEARCH_SITE_HOST_2) {
+    return colorSearch2(searchText, SEARCH_SITE_HOST_2, MAX_RESULTS);
+  } else {
+    return null;
+  }
 
-const extractLinkToValues = (responseText) => {
-  const resultSet = [];
-  const $ = cheerio.load(responseText, {
-    ignoreWhitespace: true
-  });
-  const results = $('.information');
-  results.each((i, el) => {
-    const text = $(el).text().replace(NEWLINE_REGEX, '');
-    const resultObject = {
-      range: RANGE_REGEX.exec(text)[1].replace(TRAILING_SPACE_REGEX, ''),
-      name: NAME_REGEX.exec(text)[1].replace(TRAILING_SPACE_REGEX, ''),
-      link: SEARCH_SITE_HOST + $(el).children('a').last().attr('href')
-    };
-    resultSet.push(resultObject);
-    if (i === (MAX_RESULTS -1)) return false;
-  });
-  const resultMaxNumber = results.length > MAX_RESULTS ? MAX_RESULTS : false;
-  return {resultSet, resultMaxNumber};
-};
-
-const extractRGBValues = (colourPages) => {
-  const rgbValues = [];
-  colourPages.forEach(page => {
-    const $ = cheerio.load(page, {
-      ignoreWhitespace: true
-    });
-    const rgbValue = $('.lab-result').children('span').eq(2)
-      .children('div').eq(2)
-      .children('p').eq(1)
-      .text()
-      .replace(/\s/g, '')
-      .replace(/sRGB\:/, '')
-      .split(';');
-    rgbValues.push(rgbValue);
-  });
-  return rgbValues;
-};
-
-const getRGBValues = (resultObject) => {
-  const {resultSet, resultMaxNumber} = resultObject;
-  const promises = [];
-  const links = resultSet.map(result => result.link);
-  links.forEach(link => {
-    promises.push(fetchSearch(link));
-  });
-  return Promise.all(promises)
-    .then(colourPages => {
-      const rgbValues = extractRGBValues(colourPages);
-      resultSet.forEach((result, index) => result.rgb = rgbValues[index]);
-      return {resultSet, resultMaxNumber};
-    });
-};
-
-const returnResponse = (resultObject) => {
-  return resultObject;
-};
-
-module.exports = (searchText, searchSite) => {
-  const url = constructUrl(searchText, searchSite);
-  return fetchSearch(url)
-    .then(response => extractLinkToValues(response))
-    .then(resultObject => getRGBValues(resultObject))
-    .then(resultObject => resultObject);
 };
